f8e802b9f2b53e5d5aa176ac2f23bc95c600865d
jaong <jioh035@kakao.com>
2026-06-29 10:19:37 +0900

    create lv1-4

--- src/com/App.java
+++ src/com/App.java
@@ -5,37 +5,44 @@ import java.util.Scanner;
 public class App {
     static void main() {
         Scanner sc = new Scanner(System.in);
+        while(true) {
+            System.out.print("첫 번째 숫자를 입력하세요. : ");
+            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+            int number1 = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요. : ");
+            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
 
-        System.out.print("첫 번째 숫자를 입력하세요. : ");
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        int number1 = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요. : ");
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+            int number2 = sc.nextInt();
+            System.out.print("사칙연산 기호를 입력하세요( + , - , *, /) : ");
+            // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+            sc.nextLine();
+            String symbols = sc.nextLine();
 
-        int number2 = sc.nextInt();
-        System.out.print("사칙연산 기호를 입력하세요( + , - , *, /) : ");
-        // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
-        sc.nextLine();
-        String symbols = sc.nextLine();
-
-        int result = 0;
-        switch (symbols.trim()) {
-            case "+": result = number1 + number2; break;
-            case "-": result = number1 + number2; break;
-            case "*": result = number1 + number2; break;
-            case "/": {
-                if (number2 == 0 ) {
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                    return;
-                } else {
-                    result = number1 / number2 ;
-                    break;
+            int result = 0;
+            // 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.
+            switch (symbols.trim()) {
+                case "+": result = number1 + number2; break;
+                case "-": result = number1 - number2; break;
+                case "*": result = number1 * number2; break;
+                case "/": {
+                    if (number2 == 0 ) {
+                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                        return;
+                    } else {
+                        result = number1 / number2 ;
+                        break;
+                    }
                 }
+                default: System.out.println("올바른 연산자를 입력하세요."); continue;
+
             }
 
-        }
+            System.out.println("결과 : " + result);
 
-        System.out.println("결과 :" + result);
+            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료) : ");
+            String exit = sc.nextLine();
+            if (exit.equals("exit")) break;
+        }
 
     }
 }
