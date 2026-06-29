74344f6ee7a0e3aac8abb6358277c1c78076618a
jaong <jioh035@kakao.com>
2026-06-29 10:42:41 +0900

    create lv1-5

--- src/com/App.java
+++ src/com/App.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 public class App {
     static void main() {
+        // 연산의 결과를 저장할 수 있도록 적합한 타입의 배열을 생성합니다.
+        int[] resultArr = new int[10];
+        // 연산의 결과를 비어있는 곳에 저장하기 위해 저장할 때마다 count 합니다.
+        int count = 0;
+
         Scanner sc = new Scanner(System.in);
         while(true) {
             System.out.print("첫 번째 숫자를 입력하세요. : ");
@@ -39,9 +44,30 @@ public class App {
 
             System.out.println("결과 : " + result);
 
-            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료) : ");
-            String exit = sc.nextLine();
-            if (exit.equals("exit")) break;
+            //현재 저장된 index가 마지막(9)라면 가장 먼저 저장된 결과 값이 삭제 되고 새로운 결과 값이 마지막 index에 저장될 수 있도록 구현합니다.
+            if (count == 9) {
+                for (int i = 0; i < 10; i ++){
+                    if (i == 9) {
+                        resultArr[i] = result;
+                    } else {
+                        resultArr[i] = resultArr[i+1];
+                    }
+                }
+            } else {
+                // 연산의 결과를 배열에 저장합니다.
+                resultArr[count] = result;
+                //index를 증가시킵니다.
+                count++;
+            }
+
+            for (int i : resultArr) {
+                System.out.print(i);
+            }
+            System.out.println();
+
+//            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료) : ");
+//            String exit = sc.nextLine();
+//            if (exit.equals("exit")) break;
         }
 
     }
